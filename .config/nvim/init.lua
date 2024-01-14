8e2d5228e49ede2494c92444d6049cf5243464a2
wilhelmagren <wilhelmagren98@gmail.com>
2024-01-14 10:11:05 +0100

    [config] boilerplate templates for neovim and alacritty

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+-- Enable relative line numbers.
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+-- Enable mouse support for copy/paste.
+vim.opt.mouse = 'a'
+
+-- Indentation using spaces.
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.textwidth = 90
+
+-- Set the offset for when buffer starts moving.
+vim.opt.scrolloff = 12
