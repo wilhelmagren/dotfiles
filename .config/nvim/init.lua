e4414d7a042fca8166dec198319302a8241397a8
wilhelmagren <wilhelmagren98@gmail.com>
2025-05-21 00:20:24 +0200

    feat: relative line number changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,11 +7,15 @@ require('completions')
 require('statusline')
 require('telescope')
 require('treesitter')
-require('greeter')
 
--- Enable relative line numbers.
+-- Enable relative line numbers combined with an
+-- absolute line number on the current cursorline.
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+
+-- Set the offset for when buffer starts moving.
+vim.opt.scrolloff = 10
 
 -- Enable mouse support for copy/paste.
 vim.opt.mouse = 'a'
@@ -22,9 +26,6 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.textwidth = 90
-
--- Set the offset for when buffer starts moving.
-vim.opt.scrolloff = 12
 
 vim.keymap.set('', '<M-e>', toggle_tree)
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = false })
